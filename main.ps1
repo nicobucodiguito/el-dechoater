0d8408e233cfd7112441944a912edf201b1f8675
nicobucodiguito <nicocobucoco@gmail.com>
2023-08-15 11:54:52 -0300

    first commit

---
+++ main.ps1
@@ -0,0 +1,12 @@
+﻿Add-Type -assembly System.Windows.Forms
+
+# Ventana principal
+$main_form = New-Object System.Windows.Forms.Form
+$main_form.Text ='El Dechoater'
+$main_form.Width = 550
+$main_form.Height = 200
+$main_form.AutoSize = $true
+
+
+# Ejecucion de GUI
+$main_form.ShowDialog()
