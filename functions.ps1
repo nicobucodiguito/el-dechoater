403ae708048d8c161f017392f3acec77c4605dc0
nicodiguito <nicocobucoco@gmail.com>
2023-09-02 03:02:33 -0300

    cuchau

--- functions.ps1
+++ functions.ps1
@@ -2,29 +2,60 @@ function errorMessage { # Print on console the error in case there is one
     Write-Host "Una poronga este script, no anda nada, el error es:`n $error" -ForegroundColor Red
 }
 
+function successPopup {
+    $NewForm = New-Object Windows.Forms.Form
+    $NewForm.Size = New-Object Drawing.Size @(400,100)
+    $NewForm.StartPosition = "CenterScreen"
+    $Label = New-Object System.Windows.Forms.Label
+    $Label.Text = "Costo pero se pudo"
+    $Label.Location  = New-Object System.Drawing.Point(20,20)
+    $LabeL.AutoSize = $true
+    $NewForm.Controls.Add($Label)
+    $NewForm.ShowDialog()
+}
+
+function errorPopup {
+    $NewForm = New-Object Windows.Forms.Form
+    $NewForm.Size = New-Object Drawing.Size @(500,200)
+    $NewForm.StartPosition = "CenterScreen"
+    $Label = New-Object System.Windows.Forms.Label
+    $Label.Text = "Una poronga este script, el error es:`n $error"
+    $Label.Location  = New-Object System.Drawing.Point(20,20)
+    $Label.AutoSize = $true
+    $NewForm.Controls.Add($Label)
+    $NewForm.ShowDialog()
+}
+
 # Windows Defender
 function disableDefender { # Disable Windows Defender through a registry key
     Try {
         New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value 1 -PropertyType "DWord" -ErrorAction Stop
+        successPopup
+
     }
     Catch {
         errorMessage
+        errorPopup
+        $error.Clear()
     }
 }
 
 function enableDefender { # Delete the registry key previously created
     Try {
         Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -ErrorAction Stop
+        successPopup
         # ErrorAction Stop makes Non-terminating errors trigger the catch condition
     }
     Catch {
         errorMessage
+        errorPopup
+        $error.Clear()
     }
 }
 
 # Start Menu Web Search
 
-function disableStartMenuWebSearch { # Disable Bing web search on Start Menu through registry keys, if Registry Keys exist, change their values so web search is disabled
+function disableExplorerWebSearch { # Disable Bing web search on Start Menu through registry keys, if Registry Keys exist, change their values so web search is disabled
     Try {
         New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Value 0 -PropertyType "DWord" -ErrorAction Stop # For W10 ver.1909 or older
     }
@@ -39,19 +70,26 @@ function disableStartMenuWebSearch { # Disable Bing web search on Start Menu thr
     }
     Try {
         New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "DisableSearchBoxSuggestions" -Value 1 -PropertyType "DWord" -ErrorAction Stop # For newer W10 versions
+        successPopup
     }
     Catch {
         Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "DisableSearchBoxSuggestions" -Value 1
+        errorMessage
+        errorPopup
+        $error.Clear()
     }
 }
 
-function enableStartMenuWebSearch { #Reenable and delete previous changes
+function enableExplorerWebSearch { #Reenable and delete previous changes
     Try {
         Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Value 1
         Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "CortanaConsent" -Value 1
         Remove-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "DisableSearchBoxSuggestions"
+        successPopup
     }
     Catch {
         errorMessage
+        errorPopup
+        $error.Clear()
     }
 }
--- main.ps1
+++ main.ps1
@@ -1,18 +1,67 @@
 ﻿# Modules
 Import-Module -Name '.\functions.ps1' -Force
-
 Add-Type -Assembly System.Windows.Forms
+
+
+#Background image
+$file = (get-item '.\cuchau.jpg')
+$img = [System.Drawing.Image]::Fromfile($file)
+[System.Windows.Forms.Application]::EnableVisualStyles()
+$pictureBox = new-object Windows.Forms.PictureBox
+$pictureBox.Width =  $img.Size.Width;
+$pictureBox.Height =  $img.Size.Height;
+$pictureBox.Image = $img;
+
 
 # Main GUI
 $main_form = New-Object System.Windows.Forms.Form
-$main_form.Text ='El Dechoater'
-$main_form.Width = 550
-$main_form.Height = 200
+$main_form.Text ='Dechoater 1.0'
+$main_form.Width = $pictureBox.Width
+$main_form.Height = $pictureBox.Height
 $main_form.AutoSize = $true
 
-# DEBUG
-disableStartMenuWebSearch
-# DEBUG
 
-# Ejecucion de GUI
-# $main_form.ShowDialog()
+# Buttons
+
+$disableDefenderButton = New-Object System.Windows.Forms.Button
+$disableDefenderButton.Text = "Disable Windows Defender"
+$disableDefenderButton.Location  = New-Object System.Drawing.Point(270, 20)
+$disableDefenderButton.AutoSize = $true
+$disableDefenderButton.Add_Click({
+    disableDefender
+})
+
+$enableDefenderButton = New-Object System.Windows.Forms.Button
+$enableDefenderButton.Text = "Enable Windows Defender"
+$enableDefenderButton.Location  = New-Object System.Drawing.Point(50, 20)
+$enableDefenderButton.AutoSize = $true
+$enableDefenderButton.Add_Click({
+    enableDefender
+})
+
+$disableExplorerWebSearchButton = New-Object System.Windows.Forms.Button
+$disableExplorerWebSearchButton.Text = "Disable Explorer Web Search"
+$disableExplorerWebSearchButton.Location  = New-Object System.Drawing.Point(270, 60)
+$disableExplorerWebSearchButton.AutoSize = $true
+$disableExplorerWebSearchButton.Add_Click({
+    disableExplorerWebSearch
+})
+
+$enableExplorerWebSearchButton = New-Object System.Windows.Forms.Button
+$enableExplorerWebSearchButton.Text = "Enable Explorer Web Search"
+$enableExplorerWebSearchButton.Location  = New-Object System.Drawing.Point(50, 60)
+$enableExplorerWebSearchButton.AutoSize = $true
+$enableExplorerWebSearchButton.Add_Click({
+    enableExplorerWebSearch
+})
+
+# GUI adding & drawing
+
+$main_form.Controls.Add($disableDefenderButton)
+$main_form.Controls.Add($enableDefenderButton)
+$main_form.Controls.Add($disableExplorerWebSearchButton)
+$main_form.Controls.Add($enableExplorerWebSearchButton)
+$main_form.Controls.Add($pictureBox)
+$main_form.ShowDialog()
+
+Write-Host($main_form.Width, $main_form.Height)
